77ae9728dc7dff04cebafc75e95d264ab10d1b97
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-21 22:56:15 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,35 @@
 # Write your code here.
+def dictionary
+  words = {
+    "hello" => "hi",
+    "two" => "2",
+    "to" => "2",
+    "too" => "2",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+  
+  return words
+end
+
+def word_substituter(tweet)
+  nt = tweet.split(" ")
+  
+  nt.each do |i|
+    i = "REPLACEMENT"
+  end
+    
+  return nt
+end
+
+def bulk_tweet_shortener(tweet)
+end
+
+def selective_tweet_shortener(tweet)
+end
+
+def shortened_tweet_truncator(tweet)
+end
+
