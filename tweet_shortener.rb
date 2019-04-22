fa077ee8d2d54a0e582ab00b8be918f7421de1b1
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-22 00:20:12 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -73,7 +73,7 @@ def shortened_tweet_truncator(tweet)
   puts tl
   if tl > 140
     puts "Trunicate this one: #{nt} (#{tl})"
-    tweet = "#{tweet[0..140]}"
+    tweet = "#{tweet[0..139]}"
   else
     return tweet
   end
